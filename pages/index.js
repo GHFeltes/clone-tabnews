5185efdc8a33ab2a18c9faded7fac747cea7991e
GHFeltes <134107910+GHFeltes@users.noreply.github.com>
2024-11-03 20:52:11 +0000

    corrige apontmentos de pasta

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ function Home() {
         <div>
             <h1>Oi Amor, eu e o Neneco amamos você!!</h1>
             <Image
-                src="/workspaces/clone-tabnews/public/2B3C6B58-AFD5-4D07-B50E-E603C3BEB601_1_201_a.jpeg"
+                src= "/img.jpeg"
                 width={500} 
-                height={300} 
+                height={300}
             />
         </div>
     );

4613aa87918c53abd00707bd18626bd55c1c7ddb
GHFeltes <134107910+GHFeltes@users.noreply.github.com>
2024-11-03 20:34:32 +0000

    alteracao do codigo para exibir imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 function Home() {
-    return <h1>Oi Amor, eu e o Neneco amamos voce!!</h1>
+    return (
+        <div>
+            <h1>Oi Amor, eu e o Neneco amamos você!!</h1>
+            <Image
+                src="/workspaces/clone-tabnews/public/2B3C6B58-AFD5-4D07-B50E-E603C3BEB601_1_201_a.jpeg"
+                width={500} 
+                height={300} 
+            />
+        </div>
+    );
 }
 
 export default Home;
